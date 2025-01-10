b71b17315997260c9e2e8ab2b6645c85e36a8b14
Richard Viney <richard.viney@gmail.com>
2025-01-10 15:07:22 +1300

    chore: Update path in fuzzing script

--- src/rust/dcmfx_fuzz/fuzz.sh
+++ src/rust/dcmfx_fuzz/fuzz.sh
@@ -1,5 +1,7 @@
 # Runs fuzz testing on DCMfx using AFL.
 
+set -e
+
 # Install afl.rs
 cargo install cargo-afl@0.15.13
 
@@ -14,4 +16,4 @@ find ../../../test -type f -name '*.dcm' -exec cp {} inputs \;
 cargo afl build --release
 
 # Run the fuzzing. This will run indefinitely until it is terminated.
-cargo afl fuzz -c 0 -i inputs -o outputs ../target/release/dcmfx_fuzz
+cargo afl fuzz -c 0 -i inputs -o outputs ./target/release/dcmfx_fuzz
