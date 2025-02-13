f5112e2c64f2c5fb6cd2ec656c9b649b17b095f3
Richard Viney <richard.viney@gmail.com>
2025-02-13 18:07:32 +1300

    chore: Add missing spellchecker config

--- src/rust/tmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-
-# Find all text files recursively and process them
-find . -type f -name "*.rs" | while read -r file; do
-  awk 'length($0) > 80 { print FILENAME ": " $0 }' "$file"
-done
